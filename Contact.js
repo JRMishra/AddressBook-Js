a0026ae0106af39b09fb24591f7aac310b06b13e
JR Mishra <jrmnitdgp@gmail.com>
2020-11-18 21:09:52 +0530

    [JR Mishra]Add. Contact class with required properties

---
+++ Contact.js
@@ -0,0 +1,23 @@
+class Contact
+{
+    firstname;
+    lastname;
+    address;
+    city;
+    state;
+    zip;
+    phone;
+    email;    
+
+    constructor(firstname,lastname,address,city,state,zip,phone,email)
+    {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone = phone;
+        this.email = email;
+    }
+}
